427c0b40fe6d4e01d9d5d5fe5112b63ef437d47a
Nils-Helge Garli Hegvik <nilsga@gmail.com>
2014-04-06 20:39:09 +0200

    Added some linked frameworks

--- Instabug/libInstabug.linkwith.cs
+++ Instabug/libInstabug.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libInstabug.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkWith ("libInstabug.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreTelephony AudioToolbox")]
